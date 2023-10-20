d504475fc971388a01ee67e0c1971201344c6005
Ojas Kulkarni <ojaskulkarni100@gmail.com>
2023-10-19 20:08:43 -0400

    Updated test cases for CRUD operations

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,19 +1,19 @@
-# import pytest
-# from unittest.mock import patch
-# from application import app
-# import mongomock
+import pytest
+from unittest.mock import patch
+from application import app
+import mongomock
 
-# @pytest.fixture
-# def client():
-#     with mongomock.patch():
-#         with app.test_client() as client:
-#             yield client
+@pytest.fixture
+def client():
+    with mongomock.patch():
+        with app.test_client() as client:
+            yield client
 
-# def test_get_user_profile(client):
-#     db = mongomock.MongoClient().db
+def test_get_user_profile(client):
+    db = mongomock.MongoClient().db
 
-#     db.items.insert_one(
-#         {"email":"ojaskulkarni100@gmail.com" ,"weight":90, "height":180, "target_weight":80,"goal":"75"},
-#     )
-#     response = client.get("/user_profile")
-#     assert response.status_code == 302
+    db.items.insert_one(
+        {"email":"ojaskulkarni100@gmail.com" ,"weight":90, "height":180, "target_weight":80,"goal":"75"},
+    )
+    response = client.get("/user_profile")
+    assert response.status_code == 302
--- tests/test_register.py
+++ tests/test_register.py
@@ -1,24 +1,25 @@
 import pytest
-# from application import app
+import mongomock
+from application import app
 
-# @pytest.fixture
-# def client():
-#     with mongomock.patch():
-#         with app.test_client() as client:
-#             yield client
+@pytest.fixture
+def client():
+    with mongomock.patch():
+        with app.test_client() as client:
+            yield client
 
-# def test_get_user(client):
-#     db = mongomock.MongoClient().db
-#     db.items.insert_one(
-#         {"name": "OjasTest", "pwd":"hshaksjn", "weight":90, "height":180, "target_weight":80, "start_date":"2023-10-15", "target_date":
-# "2023-11-15"},
-#     )
-#     response = client.get("/register")
-#     assert response.status_code == 200
+def test_get_user(client):
+    db = mongomock.MongoClient().db
+    db.items.insert_one(
+        {"name": "OjasTest", "pwd":"hshaksjn", "weight":90, "height":180, "target_weight":80, "start_date":"2023-10-15", "target_date":
+"2023-11-15"},
+    )
+    response = client.get("/register")
+    assert response.status_code == 200
 
-# def test_insert_user(client):
-#     # Make a POST request to the /items endpoint with a JSON payload
-#     response = client.post("/register", json={"name": "OjasTest", "pwd":"hshaksjn", "weight":"90", "height":"180", "target_weight":"80", "start_date":"2023-10-15", "target_date":
-# "2023-11-15"})
-#     # Assert the response status code is 201 Created
-#     assert response.status_code == 200
+def test_insert_user(client):
+    # Make a POST request to the /items endpoint with a JSON payload
+    response = client.post("/register", json={"name": "OjasTest", "pwd":"hshaksjn", "weight":"90", "height":"180", "target_weight":"80", "start_date":"2023-10-15", "target_date":
+"2023-11-15"})
+    # Assert the response status code is 201 Created
+    assert response.status_code == 200
